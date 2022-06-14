30602c5c123f4afd0c2235832eca105e446e15cf
tomazvila <tomazvila@outlook.com>
2022-06-14 17:15:25 +0300

    Rename dotfiles directory

---
+++ dot/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "tomazvila";
+  home.homeDirectory = "/home/tomazvila";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+    };
+  };
+  ssh -t host zsh
+}
